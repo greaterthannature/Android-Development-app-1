7eb2c8f1e2c56f9a3d0616f5fb0515292d424805
David Thomson <davidthomson123@hotmail.com>
2022-06-17 17:17:32 +0100

    Initial code

---
+++ app/src/main/java/uk/co/funkimunky/myfirstapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package uk.co.funkimunky.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val tvMyTextView = findViewById<TextView>(R.id.textView)
+        btnClickMe.setOnClickListener {
+            btnClickMe.text = "You Clicked Me"
+            tvMyTextView.text = "123456789"
+        }
+
+
+
+
+    }
+}
